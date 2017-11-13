6ecf9a96961c59ca41c02f3395ea57453e8b6f81
isaac <issaxite@gmail.com>
2017-11-14 03:04:05 +0800

    修改打包目录为根目录

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -63,7 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      template: 'index.html',
+      template: 'template.html',
       inject: true,
       minify: {
         removeComments: true,
--- config/index.js
+++ config/index.js
@@ -50,11 +50,11 @@ module.exports = {
   
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsRoot: path.resolve(__dirname, '../'),
+    assetsSubDirectory: 'asset',
     assetsPublicPath: '/',
 
     /**
