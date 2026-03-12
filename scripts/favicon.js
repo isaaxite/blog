e883676e9753e7715132d063232c8361ff069ad9
isaaxite <isaacgun@outlook.com>
2026-03-13 04:37:47 +0800

    build: favicon inject script

--- scripts/favicon.js
+++ scripts/favicon.js
@@ -1,9 +1,30 @@
-// 在 <head> 中插入自定义的 favicon 链接
-hexo.extend.filter.register('theme_inject', function(injects) {
-  // 插入到 head 的末尾
-  injects.head.raw('custom-favicon', [
-    '<link rel="icon" href="/favicon.ico" sizes="32x32">',
-    '<link rel="icon" href="/icon.svg" type="image/svg+xml">',
-    '<link rel="apple-touch-icon" href="/apple-touch-icon.png">',
-  ].join('\n'), {}, { cache: true });
+hexo.extend.injector.register('head_begin', () => {
+  const faviconConf = hexo.config.favicon;
+
+  if (!faviconConf) {
+    return;
+  }
+
+  const linkTagArr = [];
+
+  for (const favicon of faviconConf) {
+    const { rel, href, ...attrs } = favicon;
+    if (!favicon.href) {
+      hexo.log.warn(`Favicon config error: Missing 'href' in ${JSON.stringify(favicon)}`);
+      continue;
+    }
+
+    const strArr = [
+      `rel="${rel || 'icon'}"`,
+      `href="${href}"`,
+    ];
+
+    for (const [key, value] of Object.entries(attrs)) {
+      strArr.push(`${key}="${value}"`);
+    }
+
+    linkTagArr.push(`<link ${strArr.join(' ')}>`);
+  }
+
+  return linkTagArr.join('\n');
 });
